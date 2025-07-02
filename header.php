6f4e236664249b6edcd67d78284d8b0a433e2e00
Francyelli Silva <francyelli.silva@uniao.local>
2025-07-02 17:27:31 -0300

    Iniciando projeto

---
+++ header.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Exercício PHP</title>
+    <!-- Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/exercicio_php/assets/css/style.css" rel="stylesheet">
+
+    </head>
+
+    <body>
+        <div class="container">
+ 
